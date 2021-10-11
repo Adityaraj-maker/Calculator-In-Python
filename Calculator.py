8fe4225dc043f1c7f082a2a8ce325d6a9b0396af
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2021-10-12 00:29:01 +0530

    Now it is much cleaner.

--- Calculator.py
+++ Calculator.py
@@ -1,20 +1,28 @@
-# Program to creat four function calculator.
-answer=0
-num1=int(input("Enter your first number:"))
-num2=float(input("Enter your second number"))
-operator=input("Enter the Operator (+,-,*,/):")
-if operator=="+":
-    answer = num1+num2
-elif operator == "-":
-    answer = num1-num2
-elif operator=="*":
-    answer=num1*num2
-elif operator=="/":
-    if num2==0:
-        print("Enter a value other than 0")
-    else:
-        answer=num1/num2
-else:
-    print("Error")
-print("Answer = ",answer)
+add=lambda x,y:x+y
+div=lambda x,y:x/y
+sub=lambda x,y:x-y
+multipy=lambda x,y:x*y
+square=lambda x:x*x
+
+x=float(input("\nEnter first number: "))
+y=float(input("Enter second number: "))
+operation=input("Enter operation symbol: ")
+
+if operation=="+":
+    print(f'Answer is {add(x,y)}')
+
+elif operation=='-':
+    print(f'Answer is {sub(x,y)}')
+
+elif operation=='/':
+    try:print(f'Answer is {div(x,y)}')
+    except ZeroDivisionError:print("Please enter a value other than 0")
+
+elif operation=='*':
+    print(f'Answer is {multipy(x,y)}')
+
+elif operation=="sq":
+    print(f"square {x} is {square(x)}")
+
+else:print("Invalid operator, Only valid + - / * sq")
 
